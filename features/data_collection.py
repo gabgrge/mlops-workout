86dca253053eb4cced7aa726ff70f2bc5c1703b3
gabgrge <gabriel.george@efrei.net>
2024-02-06 23:48:06 +0100

    Data Processing

--- features/data_collection.py
+++ features/data_collection.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 import re
 
@@ -71,3 +72,61 @@ def filter_exercise_data(workout_data: pd.DataFrame, exercises: pd.DataFrame, ou
     except Exception as e:
         data_collection_logger.error(f"An error occurred filtering exercise data: {e}")
         return pd.DataFrame()
+
+
+def enrich_workout_data(workout_data: pd.DataFrame, filtered_exercises: pd.DataFrame, output_path: str) -> pd.DataFrame:
+    try:
+        data_collection_logger.info("Enriching workout data...")
+
+        # Merge the workout data with the filtered exercise data
+        enriched_workouts = pd.merge(workout_data, filtered_exercises, how='left', left_on='EXERCISE', right_on='Title')
+
+        # Drop unnecessary columns
+        enriched_workouts = enriched_workouts.drop(columns=['Title', 'Desc', 'RatingDesc'])
+
+        # Save the enriched workout data to current directory
+        enriched_workouts.to_csv(f"{output_path}/enriched_workout_data.csv", index=False, header=True)
+
+        data_collection_logger.info("Workout data enriched.")
+        return enriched_workouts
+
+    except Exception as e:
+        data_collection_logger.error(f"An error occurred enriching workout data: {e}")
+        return pd.DataFrame()
+
+
+def aggregate_workout_data(enriched_workouts: pd.DataFrame, output_path: str) -> [pd.DataFrame, pd.DataFrame]:
+    try:
+        data_collection_logger.info("Aggregating workout data...")
+
+        # Aggregate the enriched workout data by date, workout, and exercise
+        workout_day_exercises = enriched_workouts \
+            .groupby(["DATE", "WORKOUT", "EXERCISE"], sort=False) \
+            .agg(Type=("Type", "first"),
+                 BodyPart=("BodyPart", "first"),
+                 Equipment=("Equipment", "first"),
+                 Level=("Level", "first"),
+                 Rating=("Rating", "first"),
+                 NB_SETS=("SET", len),
+                 AVERAGE_REPS=("NB_REPS", "mean"),
+                 AVERAGE_WEIGHT=("WEIGHT", lambda x: np.average(x, weights=enriched_workouts.loc[x.index, "NB_REPS"])),
+                 MAX_WEIGHT=("WEIGHT", "max")) \
+            .reset_index()
+
+        # Aggregate the enriched workout data by date and workout
+        workout_days = workout_day_exercises \
+            .groupby(["DATE", "WORKOUT"], sort=False) \
+            .agg(NB_EXERCISES=("EXERCISE", len),
+                 NB_SETS=("NB_SETS", "sum")) \
+            .reset_index()
+
+        # Save the aggregated workout data to current directory
+        workout_day_exercises.to_csv(f"{output_path}/workout_day_exercises.csv", index=False, header=True)
+        workout_days.to_csv(f"{output_path}/workout_days.csv", index=False, header=True)
+
+        data_collection_logger.info("Workout data aggregated.")
+        return workout_day_exercises, workout_days
+
+    except Exception as e:
+        data_collection_logger.error(f"An error occurred aggregating workout data: {e}")
+        return pd.DataFrame(), pd.DataFrame()
--- scheduler.py
+++ scheduler.py
@@ -2,7 +2,8 @@ import schedule
 import time
 
 from logger_config import configure_logger
-from features.data_collection import collect_workout_data, fetch_exercise_data, filter_exercise_data
+from features.data_collection import (collect_workout_data, fetch_exercise_data, filter_exercise_data,
+                                      enrich_workout_data, aggregate_workout_data)
 
 # Configure logging for the scheduler
 scheduler_logger = configure_logger(name='scheduler')
@@ -11,7 +12,8 @@ scheduler_logger = configure_logger(name='scheduler')
 def data_ingestion_job():
     scheduler_logger.info("Running data ingestion job...")
 
-    workout_data = collect_workout_data(input_path='./data/workouts', output_path='./data/current')
+    workout_data = collect_workout_data(input_path='./data/workouts',
+                                        output_path='./data/current')
     if workout_data.empty:
         scheduler_logger.error("No workout data was collected.")
         return
@@ -21,9 +23,24 @@ def data_ingestion_job():
         scheduler_logger.error("No exercise data was fetched.")
         return
 
-    filtered_exercise_data = filter_exercise_data(workout_data=workout_data, exercises=exercise_data, output_path='./data/current')
+    filtered_exercise_data = filter_exercise_data(workout_data=workout_data,
+                                                  exercises=exercise_data,
+                                                  output_path='./data/current')
     if filtered_exercise_data.empty:
         scheduler_logger.error("No exercise data was filtered.")
+        return
+
+    enriched_workout_data = enrich_workout_data(workout_data=workout_data,
+                                                filtered_exercises=filtered_exercise_data,
+                                                output_path='./data/current')
+    if enriched_workout_data.empty:
+        scheduler_logger.error("No workout data was enriched.")
+        return
+
+    workout_day_exercises, workout_days = aggregate_workout_data(enriched_workouts=enriched_workout_data,
+                                                                 output_path='./data/current')
+    if workout_day_exercises.empty and workout_days.empty:
+        scheduler_logger.error("No workout data was aggregated.")
         return
 
     scheduler_logger.info("Data ingestion job complete.")
