15d62a1ac01b8f6e8dbaeb05d90740f9d11ccc22
gabgrge <gabriel.george@efrei.net>
2024-02-06 15:06:46 +0100

    Application development

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/workouts')
+def workouts():
+    # Load the workout data
+    workouts = pd.read_csv('data/current/workout_data.csv')
+
+    # Convert the DataFrame to JSON and return it
+    return jsonify(workouts.to_dict(orient='records'))
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
