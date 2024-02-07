8ae6780b9b295b4c57d86c6dc2daf65aabc43fc7
gabgrge <gabriel.george@efrei.net>
2024-02-07 20:35:58 +0100

    Updated requirements.txt

--- src/app/test_end_to_end.py
+++ src/app/test_end_to_end.py
@@ -6,6 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 from .app import app
 
